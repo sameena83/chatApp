90e79cd676b365252ee7136c306ed38212071afb
sameena <sameenah.tm@gmail.com>
2021-07-06 09:34:02 +0200

    first commit

---
+++ src/MovieList.js
@@ -0,0 +1,14 @@
+import React ,{useState} from 'react'
+
+const MovieList = () => {
+    const[movies,setMovies]=useState([
+
+    ]);
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default MovieList
